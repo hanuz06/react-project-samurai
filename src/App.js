1797b004c05b030c2c7d898d3acd79cff423bd97
Andrey <lee1975uz@gmail.com>
2019-08-15 13:35:51 -0500

    Moved data to index.js and passed it down the tree via props

--- src/App.js
+++ src/App.js
@@ -7,14 +7,15 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
 
 const App = (props) => {
+
     return (
         <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
                 <div class='app-wrapper-content'>
-                    <Route path='/dialogs' component={Dialogs}/>
-                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' render={ () => <Dialogs dialogs={props.dialogs} messages={props.messages} /> }/>
+                    <Route path='/profile' render={ () => <Profile posts={props.posts} /> }/>
                 </div>
             </div>
         </BrowserRouter>)
